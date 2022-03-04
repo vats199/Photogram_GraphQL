49ec8cd0b11667f576540233e1eac53802d1bce2
vats199 <vatsalp.tcs@gmail.com>
2022-03-04 13:00:27 +0530

    ok

--- REST-API/controllers/feed.js
+++ REST-API/controllers/feed.js
@@ -5,32 +5,30 @@ const fs = require('fs');
 const path = require('path');
 const User = require('../models/user');
 
-exports.getPosts = (req, res, next) => {
+exports.getPosts = async (req, res, next) => {
   const currentPage = req.query.page || 1;
   const perPage = 2;
-  let totalItems;
-  Post.find().countDocuments()
-  .then(count => {
-    totalItems = count;
-    return Post.find()
-      .skip((currentPage - 1) * perPage)
-      .limit(perPage);
-  
-  })
-  .then(posts => {
-    res.status(200).json({ message: 'Fetched posts successfully!', 
-                           posts: posts, 
-                           totalItems: totalItems 
-                          })
-  })
-  .catch(err=> {
+
+  try {
+    
+    const totalItems = await Post.find().countDocuments();
+   
+    const posts = await Post.find()
+                            .populate('creator')
+                            .skip((currentPage - 1) * perPage)
+                            .limit(perPage);
+    
+      res.status(200)
+         .json({ message: 'Fetched posts successfully!', 
+                 posts: posts, 
+                 totalItems: totalItems 
+              });
+  } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
     }
     next(err);
-  })
-  
-
+  }
 };
 
 exports.createPost = (req, res, next) => {
