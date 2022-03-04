becced7457f48504eb3c70362c1e81b206ffaf82
vats199 <100670935+vats199@users.noreply.github.com>
2022-03-04 09:33:10 +0530

    Update auth.js

--- REST-API/controllers/auth.js
+++ REST-API/controllers/auth.js
@@ -5,7 +5,7 @@ const { validationResult } = require('express-validator');
 exports.signup = (req,res,next) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
-        const error = new Error('Validation failed!!!');
+        const error = new Error('Validation failed!');
         error.statusCode = 422;
         error.data = errors.array();
         throw error;
