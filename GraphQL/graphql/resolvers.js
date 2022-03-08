647d92fd95ca301412df0797ba198f2e58579ebc
vats199 <vatsalp.tcs@gmail.com>
2022-03-08 17:05:35 +0530

    ok

--- GraphQL/graphql/resolvers.js
+++ GraphQL/graphql/resolvers.js
@@ -1,6 +1,8 @@
 const User = require('../models/user');
+const Post = require('../models/post');
 const bcrypt = require('bcryptjs');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
     createUser: async function({ userInput }, req) {
@@ -31,5 +33,52 @@ module.exports = {
         })
         const createdUser = await user.save()
         return { ...createdUser._doc, _id: createdUser._id.toString()}
+    },
+    login: async function({email, password}) {
+        const user = await User.findOne({email: email});
+        if(!user){
+            const error = new Error('User not found!');
+            error.code = 401;
+            throw error;
+        }
+        const isEqual = await bcrypt.compare(password, user.password);
+        if(!isEqual) {
+            const error = new Error('Password is Incorrect!');
+            error.code = 401;
+            throw error;
+        }
+        const token = jwt.sign({
+            userId: user._id.toString(),
+            email: user.email
+        }, 'oknownewsecret', { expiresIn: '1h' })
+        return { token: token, userId: user._id.toString() }
+    },
+    createPost: async function({ postInput }, req) {
+        const errors = [];
+        if(validator.isEmpty(postInput.title) 
+            || !validator.isLength(postInput.title,  {min: 5})) {
+            errors.push({ message: 'Title is Invalid!' });
+        }
+        if(validator.isEmpty(postInput.content) 
+            || !validator.isLength(postInput.content,  {min: 5})) {
+            errors.push({ message: 'Content is Invalid!' });
+        }
+        if(errors.length > 0) {
+            const error = new Error('Invalid Input.');
+            error.data = errors;
+            error.code = 422;
+            throw error;
+        }
+        const post = new Post({
+            title: postInput.title,
+            content: postInput.content,
+            imageUrl: postInput.imageUrl
+        });
+        const createdPost = await post.save();
+        return {...createdPost._doc, 
+                _id: createdPost._id.toString(), 
+                createdAt: createdPost.createdAt.toISOString(), 
+                updatedAt: createdPost.updatedAt.toISOString()
+            }
     }
 }
